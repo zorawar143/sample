95fd1a5033e404ab57b357b81b912920b2164dd2
unknown <zorawar.singh@dal.ca>
2019-07-05 00:45:22 -0300

    adding sample files

---
+++ script/sampleScript.js
@@ -0,0 +1,28 @@
+alert("Inside sample script");
+var car1 = {    
+    model: "BMW",
+    Arrel: "gh",
+
+    ype: "new",
+    speed: 200,
+    engine: {
+    Model: "Naruti",
+    gear: 4,
+    piston: [
+        {model1: "BMW1"},
+        {model2: "BMW2"},
+        "gbjhgh",
+    ]    
+       },
+     drive: function(){return "drive";}
+};
+
+var aray= [
+
+    "size",
+    900,
+    {Age: 90},
+    {color : "green"},
+    function(){return "drive";}
+              ];
+    console.log(car1);
